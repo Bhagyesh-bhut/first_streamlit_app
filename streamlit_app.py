2bc645b41f3477fdfa51b64bbe65ae389e528a4a
Bhagyesh-bhut <114057554+Bhagyesh-bhut@users.noreply.github.com>
2023-05-04 16:20:23 +0530

    Update streamlit_app.py
    Fixed Typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,28 +2,13 @@
 import streamlit as st
 
 # set the app's title
-st.title("Text Elements")
+st.title("My Parents healthy Diner")
  
 # header
-st.header("Header in Streamlit")
+st.header("Breakfast Menu")
+
+st.text("Omega 3 & Blueberry Oatmeal")
+st.text("Kale , Spinach & Rocket Smmothie")
+st.text("Hard-Boiled Free Ege")
  
-# subheader
-st.subheader("Subheader in Streamlit")
- 
-# markdown
-# display text in bold formatting
-st.markdown("**AskPython** is an awesome website!")
-# display text in italic formatting
-st.markdown("Visit _askpython.com_ to learn from various Python tutorials.")
- 
-# code block
-code = '''
-def add(a, b):
-    print("a+b = ", a+b)
-'''
-st.code(code, language='python')
- 
-# latex
-st.latex('''
-(a+b)^2 = a^2 + b^2 + 2*a*b
-''')
+
